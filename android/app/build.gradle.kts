fcd69f1d403dd399b105cbcb65d17de1c1fbc9e9
Z User <z@container>
2026-07-08 21:33:32 +0000

    fix: upgrade Gradle 8.14, AGP 8.11.1, Kotlin 2.2.20, NDK 27.0.12077973
    - Upgrade Gradle to 8.14 as required by latest Flutter stable
    - Upgrade AGP to 8.11.1 for compileSdk 36 compatibility
    - Upgrade Kotlin to 2.2.20 as required by latest Flutter
    - Use NDK 27.0.12077973 matching app_links plugin requirement

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.arabtweets.arabtweets"
     compileSdk = 36
-    ndkVersion = "26.1.10909125"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
